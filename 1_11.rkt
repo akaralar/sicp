c40af07d6998ce9debeba42c5de0a75931fd5548
Ahmet Karalar <akaralar@gmail.com>
2014-12-18 21:32:51 +0200

    finish second part in 1.11

--- 1_11.rkt
+++ 1_11.rkt
@@ -5,3 +5,18 @@
         (else (+ (f (- n 1)) 
                  (* 2 (f (- n 2)))
                  (* 3 (f (- n 3)))))))
+
+(define (ff n)
+  (f-iter 2 1 0 n 0))
+
+(define (f-iter a b c n count) 
+  (cond ((< (- n 3) 0) n)
+        ((= (- n 3) count) (func a b c))
+        (else (f-iter (func a b c)
+                     a
+                     b
+                     n
+                     (+ count 1)))))
+
+(define (func a b c) (+ a (* 2 b) (* 3 c)))
+  
