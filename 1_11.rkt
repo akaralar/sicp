834da5512987d93aaa1f4dd5329bafecfeb24678
Ahmet Karalar <akaralar@gmail.com>
2014-12-18 21:38:52 +0200

    add question to 1.11

--- 1_11.rkt
+++ 1_11.rkt
@@ -1,5 +1,12 @@
 #lang planet neil/sicp
 
+#|“Exercise 1.11.  A function f is defined by the rule that f(n) = n if n<3 
+   and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure that 
+   computes f by means of a recursive process. Write a procedure that computes 
+   f by means of an iterative process.”
+   Excerpt From: Harold Abelson and Gerald Jay Sussman with Julie Sussman. 
+   “Structure and Interpretation of Computer Programs.” iBooks. |#
+
 (define (f n)
   (cond ((< n 3) n)
         (else (+ (f (- n 1)) 
