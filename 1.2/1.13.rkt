b81237ed09011e974d5ebb7166161421e7431ec0
Ahmet Karalar <akaralar@gmail.com>
2017-09-02 21:30:46 +0300

    Add 1.13, 1.14 and 1.15

---
+++ 1.2/1.13.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+; Exercise 1.13
+; Prove that Fib(n) is the closest integer to φ^n/5^(1/2), where
+; φ =(1+5^(1/2)) / 2 . Hint: Let ψ =(1−5^(1/2)) / 2 . Use induction and the
+; definition of the Fibonacci numbers (see 1.2.2) to prove that 
+; Fib(n) = (φ^n − ψ^n) / 5^(1/2)
+
+; Answer is here: http://www.billthelizard.com/2009/12/sicp-exercise-113-fibonacci-and-golden.html
