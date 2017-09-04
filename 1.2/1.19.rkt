498f1f577f315c4068dc6b38f5c8659659ee4bf4
Ahmet Karalar <akaralar@gmail.com>
2017-09-04 09:44:53 +0300

    Change question formatting for 1.19

--- 1.2/1.19.rkt
+++ 1.2/1.19.rkt
@@ -19,6 +19,37 @@
 ; transformations, and thus we can compute T^n using successive squaring, as in
 ; the fast-expt procedure. Put this all together to complete the following
 ; procedure, which runs in a logarithmic number of steps:”
+;
+; (define (fib n)
+;   (fib-iter 1 0 0 1 n))
+; (define (square x) (* x x))
+;
+; (define (fib-iter a b p q count)
+;   (cond ((= count 0)
+;          b)
+;        ((even? count)
+;          (fib-iter
+;           a
+;           b
+;           (??) ; compute p′
+;           (??) ; compute q′
+;           (/ count 2)))
+;         (else
+;          (fib-iter
+;           (+ (* b q) (* a q) (* a p))
+;           (+ (* b p) (* a q))
+;           p
+;           q
+;           (- count 1)))))
+; 
+; We have (a, b), and transformation T,
+; after T1: (ap + aq + bq, aq + bp)
+; after T2: (ap^2 + 2apq + 2aq^2 + 2bpq + bq^2,
+;            2apq + aq^2 + bp^2 + bq^2)
+; and this should be in the form(ap′ + aq′ + bq′, aq′ + bp′) and be equal to
+; expression after T2. Solving for p′ and q′, we find
+; p′ = p^2 + q^2
+; q′ = 2pq + q^2
 
 (define (fib n)
   (fib-iter 1 0 0 1 n))
@@ -27,7 +58,7 @@
 (define (fib-iter a b p q count)
   (cond ((= count 0)
          b)
-        ((even? count)
+       ((even? count)
          (fib-iter
           a
           b
@@ -42,13 +73,4 @@
           q
           (- count 1)))))
 
-; We have (a, b), and transformation T,
-; after T1: (ap + aq + bq, aq + bp)
-; after T2: (ap^2 + 2apq + 2aq^2 + 2bpq + bq^2,
-;            2apq + aq^2 + bp^2 + bq^2)
-; and this should be in the form(ap′ + aq′ + bq′, aq′ + bp′) and be equal to
-; expression after T2. Solving for p′ and q′, we find
-; p′ = p^2 + q^2
-; q′ = 2pq + q^2
-;
 ; another solution on the web: https://kendyck.com/2005/05/13/solution-to-sicp-exercise-119/
--- 1.2/notes.rkt
+++ 1.2/notes.rkt
@@ -47,3 +47,34 @@
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+; 1.2.4 Exponentiation
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+(define (expt-i b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+(define (fast-expt b n)
+  (cond ((= n 0)
+         1)
+        ((even? n)
+         (square (fast-expt b (/ n 2))))
+        (else
+         (* b (fast-expt b (- n 1))))))
+(define (square x) (* x x))
+(define (even? n)
+  (= (remainder n 2) 0))
+
+
+; 1.2.5 Greatest Common Divisors
+
