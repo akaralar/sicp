b556a00aa7a6c54da85f98cbc4d77f5f196d1182
Ahmet Karalar <akaralar@gmail.com>
2017-09-12 20:57:59 +0300

    Add solution for 1.34

---
+++ 1.3/1.34.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+; Exercise 1.34
+; Suppose we define the procedure
+
+(define (f g) (g 2))
+
+; Then we have
+(define (square x) (* x x))
+(f square)
+
+
+(f (lambda (z) (* z (+ z 1))))
+
+(f f)
+; What happens if we (perversely) ask the interpreter to evaluate the
+; combination (f f)? Explain.
+; -----
+; It would try to apply (f f) -> (f 2) -> (2 2) which would mean
+; nothing and give an error(??)
+
--- 1.3/notes.rkt
+++ 1.3/notes.rkt
@@ -54,3 +54,20 @@
 
 (integral cube 0 1 0.01)
 (integral cube 0 1 0.001)
+
+; 1.3.2 Constructing procedures using lambda
+(lambda (x) (+ x 4))
+(lambda (x) (/ 1.0 (* x (+ x 2))))
+
+(define (pi-sum a b)
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a
+       (lambda (x) (+ x 4))
+       b))
+
+(define (integral f a b dx)
+  (* (sum f
+          (+ a (/ dx 2.0))
+          (lambda (x) (+ x dx))
+          b)
+     dx))
