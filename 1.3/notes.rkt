4e17463fff8f23d7a904a1f336b6177655816b06
Ahmet Karalar <akaralar@gmail.com>
2017-09-12 21:44:44 +0300

    Add solution for 1.35

--- 1.3/notes.rkt
+++ 1.3/notes.rkt
@@ -59,15 +59,119 @@
 (lambda (x) (+ x 4))
 (lambda (x) (/ 1.0 (* x (+ x 2))))
 
-(define (pi-sum a b)
+(define (pi-sum-l a b)
   (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
        a
        (lambda (x) (+ x 4))
        b))
 
-(define (integral f a b dx)
+(define (integral-l f a b dx)
   (* (sum f
           (+ a (/ dx 2.0))
           (lambda (x) (+ x dx))
           b)
      dx))
+
+(define (square x) (* x x))
+((lambda (x y z) (+ x y (square z))) 1 2 3)
+
+(define (f x y)
+  (define (f-helper a b)
+    (+ (* x (square a))
+       (* y b)
+       (* a b)))
+  (f-helper (+ 1 (* x y))
+            (- 1 y)))
+
+(define (f-l x y)
+  ((lambda (a b)
+     (+ (* x (square a))
+        (* y b)
+        (* a b)))
+   (+ 1 (* x y))
+   (- 1 y)))
+
+(define (f-let x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+
+(let ((x 5))
+(+ (let ((x 3))
+     (+ x (* x 10)))
+   x))
+
+(let ((x 2))
+(let ((x 3)
+      (y (+ x 2)))
+  (* x y)))
+
+(define (f-f x y)
+  (define a
+    (+ 1 (* x y)))
+  (define b (- 1 y))
+  (+ (* x (square a))
+     (* y b)
+     (* a b)))
+
+; 1.3.3 Procedures as General Methods
+(define (average x y) (/ (+ x y) 2))
+(define (search f neg-point pos-point)
+  (let ((midpoint
+         (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value) (search f neg-point midpoint))
+                ((negative? test-value) (search f midpoint pos-point))
+                (else midpoint))))))
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value)
+                (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value)
+                (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+    (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ x y))
+               1.0))
+
+(define (sqrt2 x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+(sqrt2 9)
+(sqrt2 10)
+(sqrt2 16)
