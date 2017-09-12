4fb23e49dbbb22790833c30e6b284a5ab0073108
Ahmet Karalar <akaralar@gmail.com>
2017-09-12 03:09:10 +0300

    Add solution for 1.30

--- 1.3/1.29.rkt
+++ 1.3/1.29.rkt
@@ -12,13 +12,16 @@
 ; value of the integral, computed using Simpson’s Rule. Use your procedure to
 ; integrate cube between 0 and 1 (with n = 100 and n = 1000), and compare the
 ; results to those of the integral procedure shown above.
-
+; -----
+;
+; From the book, for summation we have
 (define (sum term a next b)
   (if (> a b)
       0
       (+ (term a)
          (sum term (next a) next b))))
 
+; Defining Simpson's Rule
 (define (simpson-integral f a b n)
   (define (h) (/ (- b a) n))
   (define (y k) (f (+ a (* k (h)))))
@@ -31,7 +34,6 @@
   (* (/ (h) 3) (sum term 0 inc n))) 
 
 (define (cube x) (* x x x))
-
 (simpson-integral cube 0.0 1.0 100)  ; 0.24999999999999992
 (simpson-integral cube 0.0 1.0 1000) ; 0.2500000000000003
 
---
+++ 1.3/1.30.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+; Exercise 1.30
+; The sum procedure above generates a linear recursion. The procedure can be
+; rewritten so that the sum is performed iteratively. Show how to do this by
+; filling in the missing expressions in the following definition:
+; (define (sum term a next b)
+;   (define (iter a result)
+;     (if <??>
+;         <??>
+;         (iter <??> <??>)))
+;   (iter <??> <??>))
+; -----
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b) 
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+; For testing, we define cube and increment
+(define (cube x) (* x x x))
+(define (inc x) (+ x 1))
+
+(sum cube 1 inc 10) ; 3025
