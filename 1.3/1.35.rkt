ec2a549038dc18aa9f4f1d9e384e7b73ffc60220
Ahmet Karalar <akaralar@gmail.com>
2017-09-13 03:32:36 +0300

    Add solution for 1.39

--- 1.3/1.35.rkt
+++ 1.3/1.35.rkt
@@ -1,2 +1,33 @@
 #lang sicp
 ; Exercise 1.35
+; Show that the golden ratio φ (1.2.2) is a fixed point of the transformation 
+; x ↦ 1 + 1/x
+; and use this fact to compute φ by means of the fixed-point procedure.
+; -----
+; From 1.2.2, we see that 
+; φ = (1 + 5^(1/2)) / 2
+; Being fixed point of transformation implies
+; φ = 1 + 1/φ
+; Substituting equation from 1.2.2 for φ
+; (1 + 5^(1/2)) / 2 = 1 + 1/((1 + 5^(1/2)) / 2)
+; (1 + 5^(1/2)) / 2 = (3 + 5^(1/2)) / (1 + 5^(1/2))
+;
+; Multiplying both sides with 2φ = 1 + 5^(1/2)
+; (1 + 5^(1/2))^2 / 2 = 3 + 5^(1/2)
+; (1 + 2*5^(1/2) + 5) / 2 = 3 + 5^(1/2)
+; 3 + 5^(1/2) = 3 + 5^(1/2)
+; Hence, φ is a fixed point for x ↦ 1 + 1/x
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+    (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
---
+++ 1.3/1.39.rkt
@@ -0,0 +1,20 @@
+#lang sicp
+; Exercise 1.39
+; A continued fraction representation of the tangent function was published in
+; 1770 by the German mathematician J.H. Lambert:
+; tan x = x / (1 - (x^2 / (3 - (x^2 / (5 - ..., where x is in radians.
+; Define a procedure (tan-cf x k) that computes an approximation to the tangent
+; function based on Lambert’s formula. k specifies the number of terms to
+; compute, as in Exercise 1.37.
+; -----
+
+(define (tan-cf x k)
+  (define (n k) (if (= k 1) x (* x x)))
+  (define (d k) (- (* k 2.0) 1.0))
+  (define (iter x)
+    (if (= x k)
+        (/ (n x) (d x))
+        (/ (n x) (- (d x) (iter (+ x 1))))))
+  (iter 1))
+
+(tan-cf 1.0 10)
