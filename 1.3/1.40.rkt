86a376879d475831f78812837ff9e906b8866d07
Ahmet Karalar <akaralar@gmail.com>
2017-09-14 14:58:45 +0300

    Improve solution for 1.40

--- 1.3/1.40.rkt
+++ 1.3/1.40.rkt
@@ -20,6 +20,7 @@
        dx)))
 (define dx 0.00001)
 
+(define tolerance 0.00001)
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2))
@@ -31,38 +32,10 @@
         (try next))))
   (try first-guess))
 
-(define tolerance 0.00001)
-(define (close-enough? x y) (< (abs (- x y)) tolerance))
-
-
-(define (half-interval-method f a b)
-  (let ((a-value (f a))
-        (b-value (f b)))
-    (cond ((and (negative? a-value)
-                (positive? b-value))
-           (search f a b))
-          ((and (negative? b-value)
-                (positive? a-value))
-           (search f b a))
-          (else
-           (error "Values are not of opposite sign" a b)))))
-(define (average x y) (/ (+ x y) 2))
-
-(define (search f neg-point pos-point)
-  (let ((midpoint
-         (average neg-point pos-point)))
-    (if (close-enough? neg-point pos-point)
-        midpoint
-        (let ((test-value (f midpoint)))
-          (cond ((positive? test-value) (search f neg-point midpoint))
-                ((negative? test-value) (search f midpoint pos-point))
-                (else midpoint))))))
-
-
-(define (square x) (* x x))
-(define (cube x) (* (square x) x))
-
+; Define square and cube to
 (define (cubic a b c)
+  (define (square x) (* x x))
+  (define (cube x) (* (square x) x))
   (lambda (x)
     (+ (cube x) (* a (square x)) (* b x) c)))
 
